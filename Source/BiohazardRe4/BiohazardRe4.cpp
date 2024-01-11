0572767596de314cd807b4ecd921e5eba017b134
DESKTOP-QCG6R4C\rudtl <rudtlrdl96@gmail.com>
2024-01-11 19:21:15 +0900

    김경식 : 초기 세팅 추가

---
+++ Source/BiohazardRe4/BiohazardRe4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BiohazardRe4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BiohazardRe4, "BiohazardRe4" );
