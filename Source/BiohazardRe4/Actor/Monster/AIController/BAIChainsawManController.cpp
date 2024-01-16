db0c6e29b61e71a4e7af7797ad6b056038af4c85
EuihyeonO <yuu5666@naver.com>
2024-01-16 18:04:24 +0900

    오의현 : 리소스 추가, 전기톱 Attach

---
+++ Source/BiohazardRe4/Actor/Monster/AIController/BAIChainsawManController.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/Monster/AIController/BAIChainsawManController.h"
+
+ABAIChainsawManController::ABAIChainsawManController()
+{
+
+}
--- Source/BiohazardRe4/Actor/Monster/BChainsawMan.cpp
+++ Source/BiohazardRe4/Actor/Monster/BChainsawMan.cpp
@@ -2,34 +2,38 @@
 
 
 #include "Actor/Monster/BChainsawMan.h"
+#include "AIController/BAIChainsawManController.h"
 
 ABChainsawMan::ABChainsawMan()
 {
-	GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, -88.0f));
+	//GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, -88.0f));
+	//
+	//static ConstructorHelpers::FObjectFinder<USkeletalMesh> ChainsawManSkeletalMeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/Assets/Monster/Mesh/SK_ChainsawMan.SK_ChainsawMan'"));
+	//static ConstructorHelpers::FClassFinder<UAnimInstance> ChainsawManAnimInstaceClassRef(TEXT("/Game/Blueprints/Actor/Monster/ABP_ChainsawMan.ABP_ChainsawMan_C"));
+	//
+	//if (ChainsawManSkeletalMeshRef.Object != nullptr)
+	//{
+	//	GetMesh()->SetSkeletalMesh(ChainsawManSkeletalMeshRef.Object);
+	//}
+	//
+	//if (ChainsawManAnimInstaceClassRef.Class)
+	//{
+	//	GetMesh()->SetAnimInstanceClass(ChainsawManAnimInstaceClassRef.Class);
+	//}
 
-	static ConstructorHelpers::FObjectFinder<USkeletalMesh> ChainsawManSkeletalMeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/Assets/Monster/Mesh/SK_ChainsawMan.SK_ChainsawMan'"));
-	static ConstructorHelpers::FObjectFinder<USkeletalMesh> ChainsawSkeletalMeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/Assets/Monster/Mesh/SK_Chainsaw.SK_Chainsaw'"));
-	static ConstructorHelpers::FClassFinder<UAnimInstance> ChainsawManAnimInstaceClassRef(TEXT("/Game/Blueprints/Actor/Monster/ABP_ChainsawMan.ABP_ChainsawMan_C"));
-	
-	if (ChainsawManSkeletalMeshRef.Object != nullptr)
-	{
-		GetMesh()->SetSkeletalMesh(ChainsawManSkeletalMeshRef.Object);
-	}
-
-	if (ChainsawManAnimInstaceClassRef.Class)
-	{
-		GetMesh()->SetAnimInstanceClass(ChainsawManAnimInstaceClassRef.Class);
-	}
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> ChainsawSkeletalMeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/Assets/Monster/Mesh/Chainsaw/SK_Chainsaw.SK_Chainsaw'"));
 
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SK_Chainsaw"));
 	Weapon->SetupAttachment(GetMesh(), TEXT("ChainsawSocket"));
-
+	//
 	if (ChainsawSkeletalMeshRef.Object != nullptr)
 	{
 		Weapon->SetSkeletalMesh(ChainsawSkeletalMeshRef.Object);
 		Weapon->SetCollisionProfileName(TEXT("NoCollision"));
 	}
 
+	AIControllerClass = ABAIChainsawManController::StaticClass();
+	AutoPossessAI = EAutoPossessAI::PlacedInWorld;
 }
 
 void ABChainsawMan::BeginPlay()
