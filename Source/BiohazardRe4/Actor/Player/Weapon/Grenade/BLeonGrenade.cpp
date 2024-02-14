23426e269bff7ef81faa4e34b85942279d7ca988
승민 황 <iqmin11@gmail.com>
2024-02-14 13:08:07 +0900

    황승민 : 수류탄 기능 구현

--- Source/BiohazardRe4/Actor/Player/Weapon/Grenade/BLeonGrenade.cpp
+++ Source/BiohazardRe4/Actor/Player/Weapon/Grenade/BLeonGrenade.cpp
@@ -8,14 +8,8 @@
 
 
 ABLeonGrenade::ABLeonGrenade()
-	: IgnoreDamage(TArray<AActor*, FDefaultAllocator>())
 {
 	DamageType = UBDMGPlayerRifle::StaticClass();
-}
-
-void ABLeonGrenade::PostActorCreated()
-{
-
 }
 
 void ABLeonGrenade::BeginPlay()
@@ -38,43 +32,24 @@ void ABLeonGrenade::Tick(float DeltaTime)
 
 void ABLeonGrenade::Explosion()
 {
-	FActorSpawnParameters Params;
-	Params.Owner = this;
-
-	ExplosionCollision = GetWorld()->SpawnActor<ABCollisionObserverSphere>(Params);
-
-	ExplosionCollision->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);
-	ExplosionCollision->SetActorRelativeLocation(FVector(0, 0, 0));
-	ExplosionCollision->SetActorRelativeRotation(FRotator(0, 0, 0));
-	ExplosionCollision->SetRadius(Radius);
-		//SetActorRelativeScale3D(FVector::One() * Radius);
-
-	ExplosionCollision->CollisionEnterCallback.BindUObject(this, &ABLeonGrenade::ExplosionAttack);
-
-	ExplosionCollision->SetCollisionProfileName("Interaction");
-	ExplosionCollision->SetVisibilityCollision(true);
-	DrawDebugSphere(GetWorld(), ExplosionCollision->GetActorLocation(), Radius, 30, FColor::Green);
 	LOG_MSG(TEXT("Explosion"))
-	ExplosionCollision->Destroy();
-	Destroy();
-}
+	DrawDebugSphere(GetWorld(), GetActorLocation(), Radius, 30, FColor::Green);
 
-void ABLeonGrenade::ExplosionAttack(AActor* _OverlapActor)
-{
-	//UGameplayStatics::ApplyDamage(_OverlapActor, 100, Player->GetController(), this, DamageType);
 	UGameplayStatics::ApplyRadialDamage
 	(
-		_OverlapActor,
+		GetWorld(),
 		100,
-		ExplosionCollision->GetActorLocation(),
+		GetActorLocation(),
 		Radius,
 		DamageType,
-		IgnoreDamage,
+		IgnoreDamageActor,
 		this,
 		Player->GetController(),
 		false,
 		ECollisionChannel::ECC_EngineTraceChannel4
 	);
+
+	Destroy();
 }
 
 void ABLeonGrenade::Attack()
--- Source/BiohazardRe4/Actor/Player/Weapon/Grenade/BLeonGrenade.h
+++ Source/BiohazardRe4/Actor/Player/Weapon/Grenade/BLeonGrenade.h
@@ -17,14 +17,10 @@ class BIOHAZARDRE4_API ABLeonGrenade : public ABLeonWeapon
 public:
 	ABLeonGrenade();
 
-	virtual void PostActorCreated() override;
-
 	virtual void BeginPlay() override;
-
 	virtual void Tick(float DeltaTime) override;
+	
 	virtual void Explosion();
-	virtual void ExplosionAttack(AActor* _OverlapActor);
-
 	virtual void Attack() override;
 
 private:
@@ -32,14 +28,15 @@ private:
 	UPROPERTY(EditAnywhere, Category = "Option")
 	float LifeTime = 3.f;
 
+	UPROPERTY(EditAnywhere, Category = "Option")
+	float Radius = 300.f;
+
 	float Time = 0.f;
 
 	ABCollisionObserverSphere* ExplosionCollision = nullptr;
 	
 	bool IsThrowing = false;
-
-	float Radius = 100.f;
-
-	TArray<AActor*, FDefaultAllocator> IgnoreDamage;
+	
+	TArray<AActor*, FDefaultAllocator> IgnoreDamageActor;
 
 };
